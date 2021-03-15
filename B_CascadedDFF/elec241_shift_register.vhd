099ff5418289f51f4569173bfc7021e7af8b730a
TomBazleyUoP <78356762+TomBazleyUoP@users.noreply.github.com>
2021-03-15 13:10:55 +0000

    Change flipflops to behavioural VHDL

--- B_CascadedDFF/elec241_shift_register.vhd
+++ B_CascadedDFF/elec241_shift_register.vhd
@@ -5,96 +5,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity my_AND is
-
-	port (
-		A : in std_logic;
-		B : in std_logic;
-		Y : out std_logic
-	);
-
-end entity;
-
-architecture logic of my_AND is
-begin
-
-	Y <= A and B after 10 ps;
-	
-end architecture logic;
-
-
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity my_NOT is
-
-	port (
-		A : in std_logic;
-		Y : out std_logic
-	);
-
-end entity;
-
-architecture logic of my_NOT is
-begin
-
-	Y <= not A after 20 ps;
-	
-end architecture logic;
-
-
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity my_NAND is
-
-	port (
-		A : in std_logic;
-		B : in std_logic;
-		Y : out std_logic
-	);
-
-end entity;
-
-architecture logic of my_NAND is
-	
-	component my_NOT
-		port (
-			A : in std_logic;
-			Y : out std_logic
-		);
-	end component;
-	
-	component my_AND
-		port (
-			A : in std_logic;
-			B : in std_logic;
-			Y : out std_logic
-		);
-	end component;
-	
-	signal I : std_logic;
-	
-begin
-	
-	U1 : my_AND port map (A, B, I);
-	U2 : my_NOT port map (I, Y);
-	
-end architecture logic;
-
-
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
 entity d_flip_flop is
 
 	port (
@@ -107,45 +17,14 @@ end entity;
 
 architecture logic of d_flip_flop is
 
-	component my_NAND
-		port (
-			A : in std_logic;
-			B : in std_logic;
-			Y : out std_logic
-		);
-	end component;
-	
-	component my_NOT
-		port (
-			A : in std_logic;
-			Y : out std_logic
-		);
-	end component;
-	
-	component my_AND
-		port (
-			A : in std_logic;
-			B : in std_logic;
-			Y : out std_logic
-		);
-	end component;
-	
-	signal CI1, CI2, CI3, CI4, I1, I2, I3, QS, QB : std_logic;
-
 begin
 	
-	U1 : my_NOT port map (clk, CI1);	--using propegation delay for clk rising edge trigger
-	U2 : my_NOT port map (CI1, CI2);
-	U3 : my_NOT port map (CI2, CI3);
-	U4 : my_AND port map (clk, CI3, CI4);
-	
-	U5 : my_NOT port map (D, I1);
-	U6 : my_NAND port map (D, CI4, I2);
-	U7 : my_NAND port map (I1, CI4, I3);
-	U8 : my_NAND port map (I3, QS, QB);
-	U9 : my_NAND port map (I2, QB, QS);
-	
-	Q <= QS;
+	process(clk, D)
+	begin
+		if (rising_edge(clk)) then
+			Q <= D;
+		end if;
+	end process;
 	
 end architecture logic;
 
