191d8c9d0cb775b4aa08cce27893fcb3d6d80403
Stormy <stormymodding@gmail.com>
2021-03-15 11:14:20 +0000

    Remove obsolete variable
    As per Nick's suggestion for improving TASK A's "seven seg" architecture.

--- A_SevenSeg/seven_seg_decode.vhd
+++ A_SevenSeg/seven_seg_decode.vhd
@@ -24,42 +24,34 @@ architecture v1 of seven_seg_decode is
 begin
 
 	process(input,reset,en)
-	variable n : integer := 0;
 	begin
 	
-	
-		if (n = 0) then
+		if (reset = '1') then
 			output <= "0000000";
-			n := 1;
-		end if;
+      end if;
 
-		if (en = '1') then
 
+		elsif (en = '1') then
 			case input is
-                when "0000" => output <= "1111110";
-                when "0001" => output <= "0110000";
-                when "0010" => output <= "1101101";
-                when "0011" => output <= "1111001";
-                when "0100" => output <= "0110011";
-                when "0101" => output <= "1011011";
-                when "0110" => output <= "1011111";
-                when "0111" => output <= "1110000";
-                when "1000" => output <= "1111111";
-                when "1001" => output <= "1111011";
-                when "1010" => output <= "1110111";
-                when "1011" => output <= "0011111";
-                when "1100" => output <= "1001110";
-                when "1101" => output <= "0111101";
-                when "1110" => output <= "1001111";
-                when "1111" => output <= "1000111";
-                when others => output <= "0000000";
-            end case;
-			
+				when "0000" => output <= "1111110";
+				when "0001" => output <= "0110000";
+				when "0010" => output <= "1101101";
+				when "0011" => output <= "1111001";
+				when "0100" => output <= "0110011";
+				when "0101" => output <= "1011011";
+				when "0110" => output <= "1011111";
+				when "0111" => output <= "1110000";
+				when "1000" => output <= "1111111";
+				when "1001" => output <= "1111011";
+				when "1010" => output <= "1110111";
+				when "1011" => output <= "0011111";
+				when "1100" => output <= "1001110";
+				when "1101" => output <= "0111101";
+				when "1110" => output <= "1001111";
+				when "1111" => output <= "1000111";
+				when others => output <= "0000000";
+			end case;
 		end if;
-
-        if (reset = '1') then
-            output <= "0000000";
-        end if;
 		
 	end process;
 end v1;
